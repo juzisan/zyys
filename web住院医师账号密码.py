e73b0700aabd70487d3a2756c3da7c9ade2e1ad9
juzisan <juzisan@users.noreply.github.com>
2021-10-19 22:49:03 +0800

    21.10.19
    通过装饰器增加计算程序运行时间功能

--- web住院医师账号密码.py
+++ web住院医师账号密码.py
@@ -15,16 +15,19 @@ from shuju import url_dict
 from shuju import html_tou
 import time
 
-start_time = time.time()
-def itv2time(iItv):
-    zheng=int(iItv)
-    xiaoshu = iItv -zheng
-    h=zheng//3600
-    sUp_h=zheng-3600*h
-    m=sUp_h//60
-    sUp_m=sUp_h-60*m
-    s=sUp_m+xiaoshu
-    return ":".join(map(str,(h,m,s)))
+
+def timmer(func):
+    def warpper(*args,**kwargs):
+        print ('\033[1;32;40mstart\033[0m')
+        strat_time = time.time()
+        func()
+        seconds = time.time() - strat_time
+        m, s = divmod(seconds, 60)
+        print("\033[1;32;40mthe run time is %02d:%.6f\033[0m" %(m, s))
+    return warpper
+
+
+
 
 
 
@@ -35,16 +38,14 @@ def read_to_list(neir=None):
     values = ['red','green','magenta','chocolate','brown','deeppink',r'#000080']
 
 
-    kaitou = neir.find("[")
-    jiewei = neir.rfind("]")
-    neir = neir[kaitou:jiewei+1]
+    json_neir = neir[neir.find("["):neir.rfind("]")+1]
 
-    df = pd.read_json(neir)
+    df = pd.read_json(json_neir)
 
     df = df[['userName','loginName', 'password', 'departmentName', "subjectName"]]
-
-    df['字体'] = random.sample(values0 * df['userName'].count(), df['userName'].count())
-    df['颜色'] = random.sample(values * df['userName'].count(), df['userName'].count())
+    hangshu = df['userName'].count()
+    df['字体'] = random.sample(values0 * hangshu, hangshu)
+    df['颜色'] = random.sample(values * hangshu, hangshu)
 
     df['link'] = '&rArr;<a href="' + url_lj +df['loginName']+'&password='+df['password']+'" style="font-size:'+df['字体']+'%;color:'+df['颜色']+'">'+df['userName']+'_'+df['subjectName']+'_'+df['departmentName']+'</a>&emsp;'
 
@@ -52,20 +53,20 @@ def read_to_list(neir=None):
     return df['link'].tolist()
 
 
-
+@timmer
 def main():
-    print ('start')
     url_dl = url_dict['tou'] + url_dict['yy']#登陆链接
     url_xy = url_dict['tou'] + url_dict['xy']#学员
     url_ls = url_dict['tou'] + url_dict['ls']#老师
     url_jm = url_dict['tou'] + url_dict['jm']#教秘
     url_zr = url_dict['tou'] + url_dict['zr']#主任
     session = HTMLSession()
+    s_get = session.get
 
-    r = session.get(url_dl)#登陆医院
-    print ('login finished')
+    r = s_get(url_dl)#登陆医院
+    print ('login')
 #学员
-    r = session.get(url_xy)
+    r = s_get(url_xy)
 
     textt = r.html.find('script', containing='var userGridData',first = True ).html
     #搜索script 内容为var userGridData，返回第一个，再转符串
@@ -75,14 +76,14 @@ def main():
 
 
 #老师
-    r = session.get(url_ls)
+    r = s_get(url_ls)
     textt = r.html.find('script', containing='var userGridData',first = True ).html
     x_list = read_to_list(textt)
     print ("老师：", len(x_list))
     laoshi_str = ''.join(x_list)
 
 #教秘
-    r = session.get(url_jm)
+    r = s_get(url_jm)
     textt = r.html.find('script', containing='var userGridData',first = True ).html
     x_list = read_to_list(textt)
     print ("教秘：", len(x_list))
@@ -90,14 +91,14 @@ def main():
 
 
 #主任
-    r = session.get(url_zr)
+    r = s_get(url_zr)
     textt = r.html.find('script', containing='var userGridData',first = True ).html
     x_list = read_to_list(textt)
     print ("主任：", len(x_list))
     zhuren_str = ''.join(x_list)
 
 #合并html
-    #html_body = html_tou
+
     html_x = r'学员</th><td>'
     html_l = r'''</td></tr>
 	<tr style="border:2px  dashed;"><th>老师</th><td>'''
@@ -120,10 +121,8 @@ def main():
 
     with codecs.open('zyys.html','w','utf-8') as f2:
         f2.write(html_body)
-    print ('OK')
-    totaltime = time.time() - start_time
-    totaltime = itv2time(totaltime)
-    print (totaltime)
+
+
 
 
 if __name__ == "__main__":
