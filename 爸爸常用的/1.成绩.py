9af7dcbcb86d24b1e44099b8a7956c9a9e586849
juzisan <juzisan@users.noreply.github.com>
2021-11-28 19:27:23 +0800

    重新整理一下

--- 爸爸常用的/1.成绩.py
+++ 爸爸常用的/1.成绩.py
@@ -5,7 +5,6 @@ Created on Sat Oct 19 19:52:49 2019
 @author: Mrlily
 """
 
-
 import os
 import re
 import pandas as pd
@@ -13,156 +12,165 @@ import numpy as np
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl.styles import Alignment,Border,Side,Font
+from openpyxl.styles import Alignment, Border, Side, Font
+import time
 
 
+def timer(func):
+    """"计时器
+
+    """
+    def warpper():
+        """"计时器内部
+
+        """
+        print('\033[1;32;40mstart\033[0m')
+        time1 = time.time()
+        func()
+        seconds = time.time() - time1
+        m, s = divmod(seconds, 60)
+        print("\033[1;32;40mthe run time is %02d:%.6f\033[0m" % (m, s))
+
+    return warpper
 
 
-def duo(xlsx_nam,data_xlsx):
-    print ()
-    xlsx_nam=xlsx_nam+'班'
-    #文件名
+def duo(xlsx_nam, data_xlsx):
+    """重复部分"""
+    xlsx_nam = xlsx_nam + '班'
+    # 文件名
 
-    wb = Workbook()#建立excel文件
+    wb = Workbook()  # 建立excel文件
     thin_border = Border(left=Side(style='thin'),
                          right=Side(style='thin'),
                          top=Side(style='thin'),
-                         bottom=Side(style='thin'))#创建边框对象
+                         bottom=Side(style='thin'))  # 创建边框对象
 
-    ws = wb.create_sheet(xlsx_nam)#创建工作簿名
-    ws.page_setup.orientation = ws.ORIENTATION_LANDSCAPE#页面方向
-    ws.page_setup.paperSize = ws.PAPERSIZE_A4#纸张大小
+    ws = wb.create_sheet(xlsx_nam)  # 创建工作簿名
+    ws.page_setup.orientation = ws.ORIENTATION_LANDSCAPE  # 页面方向
+    ws.page_setup.paperSize = ws.PAPERSIZE_A4  # 纸张大小
     ws.page_margins.left = 0.25
     ws.page_margins.right = 0
     ws.page_margins.top = 0.2
     ws.page_margins.bottom = 0.2
     ws.page_margins.header = 0
-    ws.page_margins.footer = 0#设置页边距
-    ws.print_title_rows = '1:2'#设置打印标题为2行
+    ws.page_margins.footer = 0  # 设置页边距
+    ws.print_title_rows = '1:2'  # 设置打印标题为2行
 
-    #print (dffff.tail())
-    rows = dataframe_to_rows(data_xlsx)#pandas的dataframe转openpyxl的格式
-    for row in rows:#不知道为啥有空行。openpyxl的序号从1开始。delete_rows()
-        if len(row) >2:
+    # print (dffff.tail())
+    rows = dataframe_to_rows(data_xlsx)  # pandas的dataframe转openpyxl的格式
+    for row in rows:  # 不知道为啥有空行。openpyxl的序号从1开始。delete_rows()
+        if len(row) > 2:
             ws.append(row)
-    ws.column_dimensions['A'].width = 3#设置列宽
+    ws.column_dimensions['A'].width = 3  # 设置列宽
     ws.column_dimensions['B'].width = 9
-    for i in range(3,12):#最大列数目，是整数类型
+    for i in range(3, 12):  # 最大列数目，是整数类型
         ws.column_dimensions[get_column_letter(i)].width = 4
-    for i in range(12,ws.max_column):#最大列数目，是整数类型
+    for i in range(12, ws.max_column):  # 最大列数目，是整数类型
         ws.column_dimensions[get_column_letter(i)].width = 5
     ws.column_dimensions[get_column_letter(ws.max_column)].width = 6
-    for i in list(ws.rows)[0]:#生成器转list才能迭代，第一行
-        i.alignment = Alignment(wrap_text=True)#设置文本自动换行
+    for i in list(ws.rows)[0]:  # 生成器转list才能迭代，第一行
+        i.alignment = Alignment(wrap_text=True)  # 设置文本自动换行
     for row in ws.rows:
         for cell in row:
-            cell.border =thin_border#设置边框
+            cell.border = thin_border  # 设置边框
 
-    ws.insert_rows(1)#插入标题行
-    ws.merge_cells(start_row=1, start_column=1, end_row=1, end_column=ws.max_column)#合并标题行
+    ws.insert_rows(1)  # 插入标题行
+    ws.merge_cells(start_row=1, start_column=1, end_row=1,
+                   end_column=ws.max_column)  # 合并标题行
     p_title = xlsx_nam
     ws['A1'] = p_title
-    ws['A1'].alignment = Alignment(horizontal='center')#设置标题居中
-    ws['A1'].font = Font(size=20)#设置字号大小
+    ws['A1'].alignment = Alignment(horizontal='center')  # 设置标题居中
+    ws['A1'].font = Font(size=20)  # 设置字号大小
 
-    del wb['Sheet']#删除默认sheet，保存文件
-    wb.save('试卷分析' + xlsx_nam+'.xlsx')#excel写入sheat
-
+    del wb['Sheet']  # 删除默认sheet，保存文件
+    wb.save('试卷分析' + xlsx_nam + '.xlsx')  # excel写入sheat
 
 
 def chuli(xlsx_nam_chuli):
-#共有
+    # 共有
     global biao_or
     global zong_renshu
 
-    biao_or=pd.read_excel(xlsx_nam_chuli,skiprows=1,dtype={'班级':'str',} )#读excel'总分':'int'
-    biao_or=biao_or.sort_values(by =['总分'], ascending=False)
-    biao_or.index = range(1,len(biao_or)+1)#重建索引
-    bb = biao_or.tail(1).applymap(lambda x: int(re.search(r'得分/共(\d+)分', x).group(1)), na_action='ignore')
+    biao_or = pd.read_excel(xlsx_nam_chuli, skiprows=1, dtype={
+                            '班级': 'str', })  # 读excel'总分':'int'
+    biao_or = biao_or.sort_values(by=['总分'], ascending=False)
+    biao_or.index = range(1, len(biao_or) + 1)  # 重建索引
+    bb = biao_or.tail(1).applymap(lambda x: int(
+        re.search(r'得分/共(\d+)分', x).group(1)), na_action='ignore')
 
-    #biao_or.loc[biao_or.index[-1]].apply(lambda x : 6 if x.empty  else x )
-    biao_or.drop(biao_or.tail(1).index,inplace=True)
+    # biao_or.loc[biao_or.index[-1]].apply(lambda x : 6 if x.empty  else x )
+    biao_or.drop(biao_or.tail(1).index, inplace=True)
     biao_or = pd.concat([biao_or, bb], ignore_index=True)
-    biao_or.rename(columns=lieming_dict, inplace = True)
-    biaotou = biao_or.columns.values.tolist()#pandas列名转list
-#共有
+    biao_or.rename(columns=lieming_dict, inplace=True)
+    biaotou = biao_or.columns.values.tolist()  # pandas列名转list
+    # 共有
 
     ti_list = []
-    #print (biaotou)
+    # print (biaotou)
     dati = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', ]
     for k in dati:
         for j in biaotou:
-            if j.find(k) > -1 :
+            if j.find(k) > -1:
                 ti_list.append(j)
 
-    #print (ti_list)
+    # print (ti_list)
 
     zutifenxi_fenzhi = biao_or.tail(1)
-
 
     tizhi = zutifenxi_fenzhi[ti_list].T.to_dict(orient='dict')
     tizhi = tizhi.popitem()[1]
 
-    print ('题值',tizhi)
-    #tizhi = tizhi[:1]
+    print('题值', tizhi)
+    # tizhi = tizhi[:1]
 
+    biao_or.drop(biao_or.tail(1).index, inplace=True)
 
-    biao_or.drop(biao_or.tail(1).index,inplace=True)
-
-
-
-
-    shuchu=[]
+    shuchu = []
     for i in biaotou:
-        #目的是删除多余的列。生成每题分值dict
-        if i.find('.') > 0 :
+        # 目的是删除多余的列。生成每题分值dict
+        if i.find('.') > 0:
             pass
-        elif i not in ['姓名','班级','总分']:
+        elif i not in ['姓名', '班级', '总分']:
             shuchu.append(i)
     for i in shuchu:
         biaotou.remove(i)
-    biao_or=biao_or[biaotou]#去掉无用列
-    biao_or.drop(biao_or.tail(1).index,inplace=True)
-    biao_or["合计"] = biao_or["总分"].astype('float') - 100#处理扣总分
-    del biao_or['总分']#删除总分列
-    biaotou = biao_or.columns.values.tolist()
+    biao_or = biao_or[biaotou]  # 去掉无用列
+    biao_or.drop(biao_or.tail(1).index, inplace=True)
+    biao_or["合计"] = biao_or["总分"].astype('float') - 100  # 处理扣总分
+    del biao_or['总分']  # 删除总分列
+    #  biaotou = biao_or.columns.values.tolist()
 
+    for j in tizhi:  # 变成负分.astype('float')
+        biao_or[j] = biao_or[j].astype('float') - tizhi[j]
+        biao_or[j] = biao_or[j].replace(0, np.nan)
 
-    for j in tizhi: #变成负分.astype('float')
-        biao_or[j]=biao_or[j].astype('float') - tizhi[j]
-        biao_or[j]=biao_or[j].replace(0, np.nan)
-
-    #print(biao_or.dtypes)
-    biao_or=biao_or.groupby(['班级'])#按照班级分类
-    #print (biao_or)
-    huizongbiao=[]
+    # print(biao_or.dtypes)
+    biao_or = biao_or.groupby(['班级'])  # 按照班级分类
+    # print (biao_or)
+    huizongbiao = []
     for key, value in biao_or:
-
         biaotou = value.columns.values.tolist()
         biaotou.remove('班级')
-        value=value[biaotou]
-        value.index = range(1,len(value)+1)
-        mean_mean =value.mean().values.tolist()#pandas的mean出错太多了
-        mean_mean.insert(0,'平均扣分')
-        #mean_mean.insert(1,'班级')
-        #print(dict(zip(biaotou, mean_mean)))
+        value = value[biaotou]
+        value.index = range(1, len(value) + 1)
+        mean_mean = value.mean().tolist()  # pandas的mean出错太多了
+        mean_mean.insert(0, '平均扣分')
+        # mean_mean.insert(1,'班级')
+        # print(dict(zip(biaotou, mean_mean)))
 
-        mean_mean = pd.DataFrame(dict(zip(biaotou, mean_mean)),index=[0])
-        #print(mean_mean)
-        tj=pd.DataFrame([value.count()])
+        mean_mean = pd.DataFrame(dict(zip(biaotou, mean_mean)), index=[0])
+        # print(mean_mean)
+        tj = pd.DataFrame([value.count()])
 
+        tj['姓名'] = ['扣分人数']  # 添加姓名列，内容是后面的两项，姓名列改内容
+        tj = tj[biaotou]  # 按照biao_or.columns的顺序排列tj表，不排序就出错了，最后两行统计扣分人数和平均扣分
 
-        tj['姓名']=['扣分人数']#添加姓名列，内容是后面的两项，姓名列改内容
-        tj=tj[biaotou]#按照biao_or.columns的顺序排列tj表，不排序就出错了，最后两行统计扣分人数和平均扣分
+        # 把统计加入写入表
 
-        #把统计加入写入表
+        value = value.append(mean_mean)
+        value = value.append(tj)  # dataframeappend用加法
 
-        value=value.append(mean_mean)
-        value=value.append(tj)#dataframeappend用加法
-
-        huizongbiao.append([key,value])#列表append不用加法
-
-
+        huizongbiao.append([key, value])  # 列表append不用加法
 
     for banji, banji_data in huizongbiao:
         print(banji)
@@ -170,34 +178,28 @@ def chuli(xlsx_nam_chuli):
     print('OK')
 
 
+# 数据区
+lieming_dict = {'客 | 一.1': '一.1', '客 | 一.2': '一.2', '客 | 一.3': '一.3', '客 | 一.4': '一.4', '客 | 一.5': '一.5',
+                '客 | 一.6': '一.6', '客 | 一.7': '一.7', '客 | 一.8': '一.8', '客 | 一.9': '一.9', '客 | 一.10': '一.10',
+                '主 | 11-18': '二.11-18', '主 | 三.19': '三.19', '主 | 三.20': '三.20', '主 | 四.21': '四.21', '主 | 四.22': '四.22',
+                '主 | 五.23': '五.23', '主 | 六.24': '六.24', '主 | 七.25': '七.25', '主 | 八.26': '八.26'}
 
 
-#数据区
-lieming_dict ={ '客 | 一.1':'一.1', '客 | 一.2':'一.2', '客 | 一.3':'一.3', '客 | 一.4':'一.4', '客 | 一.5':'一.5',
-               '客 | 一.6':'一.6', '客 | 一.7':'一.7', '客 | 一.8':'一.8', '客 | 一.9':'一.9', '客 | 一.10':'一.10',
-               '主 | 11-18':'二.11-18', '主 | 三.19':'三.19', '主 | 三.20':'三.20', '主 | 四.21':'四.21', '主 | 四.22':'四.22',
-               '主 | 五.23':'五.23', '主 | 六.24':'六.24', '主 | 七.25':'七.25', '主 | 八.26':'八.26'}
+# 数据区
 
 
-#数据区
-
-
-
-
+@timer
 def main():
-    #程序开始
-
+    """程序开始"""
     names = os.listdir(os.path.split(os.path.realpath(__file__))[0])
     names = [i for i in names if re.match('小分表.*.xlsx', i)]
 
     chuli(names[0])
 
 
-
-
 if __name__ == "__main__":
     main()
-    print ('all ok')
+    print('all ok')
 
 '''
 
--- 爸爸常用的/2.汇总表.py
+++ 爸爸常用的/2.汇总表.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
+# !/usr/bin/env python
 """
 Created on Sat Nov  5 10:50:13 2016
 
@@ -12,131 +12,138 @@ import re
 import pandas as pd
 import numpy as np
 from openpyxl import load_workbook
-#from openpyxl.compat import range
+#  from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Border, Side, Font, Alignment
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
+import time
 
+
+def timer(func):
+    """"计时器
+
+    """
+    def warpper():
+        """"计时器内部
+
+        """
+        print('\033[1;32;40mstart\033[0m')
+        time1 = time.time()
+        func()
+        seconds = time.time() - time1
+        m, s = divmod(seconds, 60)
+        print("\033[1;32;40mthe run time is %02d:%.6f\033[0m" % (m, s))
+
+    return warpper
 
 
 def duo(xlsx_nam):
-    #文件名
-    biao_or=pd.read_excel(xlsx_nam, index_col=0, skiprows = 1)#读excel
+    #  文件名
+    biao_or = pd.read_excel("试卷分析" + xlsx_nam + ".xlsx", index_col=0, skiprows=1)  # 读excel
     biao = biao_or[-2:].copy()
-    lie_neirong = xlsx_nam.replace('试卷分析','')
-    lie_neirong = re.search(r'((高湾|新抚)\d{1,2}班)', xlsx_nam).group(1)
-    biao['班级'] = lie_neirong
-    print ('duo done {}'.format(xlsx_nam))
+    biao['班级'] = xlsx_nam
+    print('duo done {}'.format(xlsx_nam))
     return biao
 
 
-
+@timer
 def main():
-    #程序开始
-    #数据区
+    #  程序开始
+    #  数据区
     names = os.listdir(os.path.split(os.path.realpath(__file__))[0])
     shanchu = [i for i in names if i.startswith('汇总表')]
     for i in shanchu:
         os.remove(i)
     names = os.listdir(os.path.split(os.path.realpath(__file__))[0])
-    print (names)
-    p_titlel = [i for i in names if re.match('^小分表 - ([\w\W]*).xlsx$', i)]
-    p_title = re.findall('^小分表 - ([\w\W]*).xlsx$', p_titlel[0])[0]
-    print (p_title)
+    print(names)
+    p_titlel = [i for i in names if re.match('^小分表 - ([\\w\\W]*).xlsx$', i)]
+    p_title = re.findall('^小分表 - ([\\w\\W]*).xlsx$', p_titlel[0])[0]
+    print(p_title)
     names = [i for i in names if i.count('试卷分析')]
 
-    names1 = [i for i in names if re.match('^试卷分析(高湾|新抚)\d{1}班.xlsx$', i)]
-    names2 = [i for i in names if re.match('^试卷分析(高湾|新抚)\d{2}班.xlsx$', i)]
+    names1 = [i for i in names if re.match('^试卷分析(高湾|新抚)\\d班.xlsx$', i)]
+    names2 = [i for i in names if re.match('^试卷分析(高湾|新抚)\\d{2}班.xlsx$', i)]
     names = names1 + names2
-
-    #数据区
-    data_hebing = map(duo,names)#[duo(i) for i in names]
+    names = [i.lstrip("试卷分析") for i in names]
+    names = [i.rstrip(".xlsx") for i in names]
+    data_hebing = map(duo, names)  # [duo(i) for i in names]
     data_hebing = pd.concat(data_hebing, ignore_index=True)
-    #data_hebing = data_hebing.set_index('班级')
-    data_hebing = data_hebing.replace(np.nan, 0)
-    #data_hebing = data_hebing.set_index('班级','姓名')
 
+    data_hebing = data_hebing.replace(np.nan, 0)
     biaotou = data_hebing.columns.values.tolist()
 
-    data_hebing=data_hebing.pivot_table(index='班级', columns='姓名',  margins=False)
+    data_hebing = data_hebing.pivot_table(
+        index='班级', columns='姓名',  margins=False)
+    data_hebing = data_hebing.loc[names]  # 单双位排序
+
     biaotou = biaotou[1:-1]
     biaotou3 = []
     for i in biaotou:
-        biaotou3.append((i,'扣分人数'))
-        biaotou3.append((i,'平均扣分'))
+        biaotou3.append((i, '扣分人数'))
+        biaotou3.append((i, '平均扣分'))
 
     data_hebing = data_hebing[biaotou3]
 
     data_hebing.index.map(lambda x: str(x) + '班')
-    print (data_hebing.index)
+    print(data_hebing.index)
 
-
-    wb = Workbook()#创建workbook实例
+    wb = Workbook()  # 创建workbook实例
     ws = wb.active
     thin_border = Border(left=Side(style='thin'),
                          right=Side(style='thin'),
                          top=Side(style='thin'),
-                         bottom=Side(style='thin'))#创建边框对象
-    ws.page_setup.orientation = ws.ORIENTATION_LANDSCAPE#页面方向
-    ws.page_setup.paperSize = ws.PAPERSIZE_A4#纸张大小
+                         bottom=Side(style='thin'))  # 创建边框对象
+    ws.page_setup.orientation = ws.ORIENTATION_LANDSCAPE  # 页面方向
+    ws.page_setup.paperSize = ws.PAPERSIZE_A4  # 纸张大小
     ws.page_margins.left = 0.1
     ws.page_margins.right = 0
     ws.page_margins.top = 0.2
     ws.page_margins.bottom = 0.2
     ws.page_margins.header = 0
-    ws.page_margins.footer = 0#设置页边距
+    ws.page_margins.footer = 0  # 设置页边距
     ws.print_title_cols = 'A:A'
-
 
     for row in ws.rows:
         for cell in row:
-            cell.border =thin_border#设置边框
-    ws.insert_rows(0)#插入标题行
-
+            cell.border = thin_border  # 设置边框
+    ws.insert_rows(0)  # 插入标题行
 
     for r in dataframe_to_rows(data_hebing, index=True, header=True):
         ws.append(r)
 
-    ws.column_dimensions['A'].width = 4#设置列宽
-    for i in range(2,ws.max_column+1):#最大列数目，是整数类型
+    ws.column_dimensions['A'].width = 4  # 设置列宽
+    for i in range(2, ws.max_column+1):  # 最大列数目，是整数类型
         ws.column_dimensions[get_column_letter(i)].width = 5
-    for i in range(3,ws.max_row+1):#最大行数目，是整数类型
+    for i in range(3, ws.max_row+1):  # 最大行数目，是整数类型
         ws.row_dimensions[i].height = 43
 
-
-    for i in range(2,ws.max_column,2):#合并表头
-        end_i = i +1
-        ws.merge_cells(start_row=2, start_column=i, end_row=2, end_column=end_i)
+    for i in range(2, ws.max_column, 2):  # 合并表头
+        end_i = i + 1
+        ws.merge_cells(start_row=2, start_column=i,
+                       end_row=2, end_column=end_i)
 
     for row in list(ws.rows)[1:]:
         for cell in row:
-            cell.border =thin_border#设置边框
+            cell.border = thin_border  # 设置边框
 
     ziti = ws['A2:'+get_column_letter(ws.max_column)+str(ws.max_row)]
-    for row in list(ziti):#单元格选择区域是tuple，转换成list就能赋值了
+    for row in list(ziti):  # 单元格选择区域是tuple，转换成list就能赋值了
         for cell in row:
-            cell.font=Font(size=11)
-            cell.alignment = Alignment(wrap_text=True)#设置文本自动换行
+            cell.font = Font(size=11)
+            cell.alignment = Alignment(wrap_text=True)  # 设置文本自动换行
 
-
-    ws['A3'] = ws['A4'].value#单元格值需要value
-    ws.delete_rows(4)#删行
+    ws['A3'] = ws['A4'].value  # 单元格值需要value
+    ws.delete_rows(4)  # 删行
     ws.merge_cells('D1:U1')
-    p_title = '数学' + p_title#'七年_________考试教学质量分析统计表（数学）'改成前面的了
+    p_title = '数学' + p_title  # '七年_________考试教学质量分析统计表（数学）'改成前面的了
     ws['D1'] = p_title
-    ws['D1'].alignment = Alignment(horizontal='center')#设置标题居中
-    ws['D1'].font = Font(size=20)#设置字号大小
+    ws['D1'].alignment = Alignment(horizontal='center')  # 设置标题居中
+    ws['D1'].font = Font(size=20)  # 设置字号大小
 
-
-    wb.save('汇总.xlsx')#excel写入sheat
-
-    #os.remove('汇总表表.xlsx')
-
-
+    wb.save('汇总.xlsx')  # excel写入sheat
 
 
 if __name__ == "__main__":
     main()
-    print ('all ok')
-
+    print('all ok')
