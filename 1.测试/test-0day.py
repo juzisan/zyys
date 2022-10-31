0a3d6e7174e3b6d1f2f8370257d2f6d7024323c0
juzisan <juzisan@users.noreply.github.com>
2022-10-31 22:22:50 +0800

    特殊的测试

--- 1.测试/test-0day.py
+++ 1.测试/test-0day.py
@@ -18,9 +18,9 @@ d_2.loc[d_2['ab'] < 60, 'ab'] = '不及格'
 d_2['行总和'] = d_2.sum(axis=1)
 print(d_2)
 s_1 = pd.Series(dtype=int, name='张三')
-s_1['一'] = 3
-s_1['二'] = 9
-s_1['三'] = 7
+s_1['一'] = 13
+s_1['二'] = 29
+s_1['三'] = 37
 # s_1.append([7])
 print(f'{s_1 = }')
 print('\033[1;32;40m start \033[0m')
