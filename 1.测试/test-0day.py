a04da3617aac2f60982de765c36b11bf6351bd94
juzisan <ch0413@live.cn>
2023-12-03 21:50:24 +0800

    Changes

--- 1.测试/test-0day.py
+++ 1.测试/test-0day.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import numpy as np
+from DrissionPage import ChromiumPage
+
+from DrissionPage.easy_set import set_paths
+
+# set_paths(browser_path=r'F:\Program Files\chrome-win')
+
+
+# page = ChromiumPage()
+# page.get('http://g1879.gitee.io/DrissionPageDocs')
+
 
 p1 = -1
 p2 = [-1, 0, 2]
@@ -10,11 +20,11 @@ for i in p2:
         print(i, '找到了')
 print(f'{p2 = }')
 
-
-d_2 = pd.DataFrame(np.random.randn(6, 3), ['班级', '人数', '姓名', '平均分', '总分', '最高分'], columns=['ab', 'bc', 'cd'])
+d_2 = pd.DataFrame(np.random.randn(6, 3), ['班级', '人数', '姓名', '平均分', '总分', '最高分'],
+                   columns=['ab', 'bc', 'cd'])
 d_2 = (d_2 * 100).round(0)
 print(d_2)
-d_2.loc[d_2['ab'] < 60, 'ab'] = '不及格'
+d_2.loc[d_2['ab'] < 60, 'ab'] = 0
 d_2['行总和'] = d_2.sum(axis=1)
 print(d_2)
 s_1 = pd.Series(dtype=int, name='张三')
@@ -26,3 +36,13 @@ print(f'{s_1 = }')
 print('\033[1;32;40m start \033[0m')
 '''
 '''
+data = [[1, 2, 3], [1, 5, 6], [7, 8, 9]]
+df = pd.DataFrame(data, columns=["a", "b", "c"],
+                  index=["owl", "toucan", "eagle"])
+print(df)
+try:
+    aa = df.groupby(by=["a"]).get_group(5)
+except KeyError:
+    aa = 'kong'
+
+print(aa)
--- 8.工作量分析/2.工作量统计.py
+++ 8.工作量分析/2.工作量统计.py
@@ -11,10 +11,12 @@ import re
 import time
 import numpy as np
 import pandas as pd
-from argument import (col_n, jiancxm_old, jiancxm_tihuan,)
+from argument import (col_n, jiancxm_old, jiancxm_tihuan, )
+
 global yue, canyn, jiancxm_old, jiancxm_tihuan
 
-canyn=[] #统计残余尿项目名称
+canyn = []  # 统计残余尿项目名称
+
 
 def k_s():
     """为了生成Series."""
@@ -33,19 +35,20 @@ def timer(func):
         seconds = time.time() - time1
         m, s = divmod(seconds, 60)
         print("\033[1;32;40mthe run time is %02d:%.6f\033[0m" % (m, s))
+
     return warpper
 
 
 def one_do(neir_str, leix_type):
     """逻辑处理部分."""
-    jishu = k_s() # 复制Series
+    jishu = k_s()  # 复制Series
     if leix_type == '门住':
-        #图文报告
+        # 图文报告
         jishu['图文报告'] = 1
-        if re.match(r'^\[(.*?),三维\]', neir_str):
+        if re.match(r'^\[(.*?),三维]', neir_str):
             jishu['三维'] = 1
             # 残尿三维不以三维结尾
-        elif re.match(r'^\[(.*?),二维\]', neir_str):
+        elif re.match(r'^\[(.*?),二维]', neir_str):
             if neir_str.count(r'卵泡测定'):
                 # print(neir_str)
                 jishu['超声检查正常(包括双胎)'] = 1
@@ -72,8 +75,7 @@ def one_do(neir_str, leix_type):
             print('缺少二维三维：', neir_str)
             jishu['超声检查正常(包括双胎)'] = 1
 
-
-        #残余尿
+        # 残余尿
         if neir_str.count(r'残余'):
             jishu['残余尿测定'] = 1
             canyn.append(neir_str)
@@ -87,27 +89,25 @@ def one_do(neir_str, leix_type):
     return jishu
 
 
-
 def do_it(file_str):
     """程序开始."""
-    global yue,canyn,jiancxm_old
+    global yue, canyn, jiancxm_old
     ori_df = pd.read_excel(io=file_str, sheet_name=0, parse_dates=['检查时间'])
-    ori_df = ori_df[['检查时间', '患者类型', '检查部位,检查方法',]]
+    ori_df = ori_df[['检查时间', '患者类型', '检查部位,检查方法', ]]
     ori_df = ori_df.sort_values(by=['检查时间'], ascending=True)
     ori_df.rename(columns={'检查部位,检查方法': '检查部位'}, inplace=True)  # 把列重命名
-    ori_df['患者类型'] = ori_df['患者类型'].replace({'住院':'门住', '门诊':'门住'})
+    ori_df['患者类型'] = ori_df['患者类型'].replace({'住院': '门住', '门诊': '门住'})
     # 把患者类型列的值重新替换一下
-    ori_df['检查时间'] = ori_df['检查时间'].dt.day # 检查时间只保留 day
-    fenzu = ori_df.groupby(['检查时间']) # 按照检查时间分组
-    jiancxm = ori_df[ori_df["患者类型"]  == "门住"]
+    ori_df['检查时间'] = ori_df['检查时间'].dt.day  # 检查时间只保留 day
+    fenzu = ori_df.groupby(['检查时间'])  # 按照检查时间分组
+    jiancxm = ori_df[ori_df["患者类型"] == "门住"]
 
     jiancxm = jiancxm["检查部位"].str.rstrip("彩超,二三维])胰脾早中晚期妊娠（右）左")
-   # jiancxm = jiancxm["检查部位"].str.rstrip(",二维")subset=["检查部位"], 
-   
+    # jiancxm = jiancxm["检查部位"].str.rstrip(",二维")subset=["检查部位"],
 
     jiancxm = jiancxm.str.lstrip("[彩超(")
-    jiancxm.replace(jiancxm_tihuan,inplace=True)
-    jiancxm.drop_duplicates( keep="first", inplace=True)
+    jiancxm.replace(jiancxm_tihuan, inplace=True)
+    jiancxm.drop_duplicates(keep="first", inplace=True)
     jiancxm = jiancxm.sort_values(ascending=True)
     jiancxm = jiancxm.to_list()
 
@@ -116,32 +116,31 @@ def do_it(file_str):
     # print(jiancxm,'\n',jiancxm_old)
     new_old = set(jiancxm).difference(set(jiancxm_old))
     print('新加：\n')
-    print(*new_old, sep = "\n") # 显示没有的检查项目
+    print(*new_old, sep="\n")  # 显示没有的检查项目
     print('\n----------\n')
     print('旧多：\n')
     old_new = set(jiancxm_old).difference(set(jiancxm))
-    print(*old_new, sep = "\n") # 多余的检查项目
+    print(*old_new, sep="\n")  # 多余的检查项目
     print('\n----------\n')
 
-
-    zonghe = [] # 准备列表生成统计
-    for i in range(1,32):
+    zonghe = []  # 准备列表生成统计
+    for i in range(1, 32):
         try:
-            jisruan = fenzu.get_group(i).apply(lambda row :one_do(row['检查部位'],row['患者类型']),axis=1)
+            jisruan = fenzu.get_group(i).apply(lambda row: one_do(row['检查部位'], row['患者类型']), axis=1)
             # groupby 对象需要用 get_group 才能调用,df用apply传递多个参数的时候要用lambda
-        except:
+        except KeyError:
             jishu = k_s()
-            print(i,'日  没上班')
+            print(i, '日  没上班')
         else:
             jishu = jisruan.sum()
-        jishu.rename(i,inplace=True) # 对 Series 重命名
+        jishu.rename(i, inplace=True)  # 对 Series 重命名
         zonghe.append(jishu)
 
-    zonghe = pd.concat(zonghe, axis=1).T # 合并表，转置表
-    zonghe.loc['总和'] = zonghe.apply(lambda x: x.sum()) # 各列求和，添加新的行
+    zonghe = pd.concat(zonghe, axis=1).T  # 合并表，转置表
+    zonghe.loc['总和'] = zonghe.apply(lambda x: x.sum())  # 各列求和，添加新的行
     print('\n----------\n')
     print(list(set(canyn)))
-    zonghe.replace(0,np.nan, inplace=True)
+    zonghe.replace(0, np.nan, inplace=True)
     print(zonghe)
     zonghe.to_excel('统计好' + yue + '月.xlsx')
 
@@ -159,11 +158,10 @@ def main():
                 print(i, '代表：  ', value)
             select_num = int(input("输入转换的序号："))
             file_n = names[select_num]
-            yue = re.search(r'\d+', file_n)[0]
-            do_it(file_n)
         else:
-            yue = re.search(r'\d+', file_n)[0]
-            do_it(names[0])
+            file_n = names[0]
+        yue = re.search(r'\d+', file_n)[0]
+        do_it(names[0])
     else:
         print('缺少文件')
 
